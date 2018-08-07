c11b32cc46ee5f187250ddbc3dfb4cd60fd53c55
JaxBBLL <showcye@foxmail.com>
2018-08-07 10:03:21 +0800

    m

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from 'vue'
 import router from './router'
 import './assets/style/reset.css'
-// import ElementUI from 'element-ui'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-
-// import 'element-ui/lib/theme-default/index.css'
-// import './assets/theme/theme/index.css'  //  小号主题
 import './assets/style/el-ui.less'
 import './assets/style/common.less'
 import 'font-awesome/css/font-awesome.min.css'  //  字体
@@ -24,7 +20,6 @@ var echarts = require('echarts/lib/echarts')
 // 引入 ECharts 其他模块
 
 require('./echarts')
-// Vue.use(ElementUI)
 Vue.use(ElementUI, { size: 'small' });
 Vue.use(utils)
 Vue.use(PublicComponents)
